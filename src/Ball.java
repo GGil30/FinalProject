0a8b885671d463a66e30374386d16d43712217be
GGil30 <gabriel.gil@menloschool.org>
2025-04-28 11:54:22 -0700

    Worked on ball movement and the process of animating the ball's movement.

--- src/Ball.java
+++ src/Ball.java
@@ -9,17 +9,49 @@ public class Ball {
     public static final int BALLSIZE = 50;
     private int x;
     private int y;
+    private int dx;
+    private int dy;
 
     public Ball(GameView window){
         this.window = window;
 
+        dx = 15;
+        dy = 15;
         ballImage = new ImageIcon("Resources/ball.png").getImage();
         x = (int) (GameView.WINDOW_WIDTH/2 - (0.5)*(BALLSIZE));
-        y = (int) (GameView.WINDOW_HEIGHT*(7.0/8));
+        y = (int) (GameView.WINDOW_HEIGHT*(7.5/8) - (0.5)*(BALLSIZE));
     }
 
     public void draw(Graphics g){
-
         g.drawImage(ballImage, x, y, BALLSIZE, BALLSIZE, window);
+    }
+
+    public void move(){
+            x += dx;
+            y += dy;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
     }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,7 @@
-public class Game {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Game implements MouseListener, MouseMotionListener, ActionListener {
 
     // Variables
     private GameView window;
@@ -7,15 +10,26 @@ public class Game {
     private Goalie goalie;
     private Scoreboard scoreboard;
     private int state;
+    private int xShotPos;
+    private int yShotPos;
+    private boolean hasClicked;
+    private static final int DELAY_IN_MILLISEC = 20;
+    Timer clock;
 
     public Game(){
         state = -1;
         this.window = new GameView(this);
+        this.window.addMouseListener(this);
+        this.window.addMouseMotionListener(this);
 
         this.ball = new Ball(window);
         this.arrow = new SpeedArrow(window);
         this.goalie = new Goalie(window);
         state = 0;
+        hasClicked = false;
+
+        clock = new Timer(DELAY_IN_MILLISEC, this);
+        clock.start();
 
         window.repaint();
     }
@@ -36,7 +50,82 @@ public class Game {
         return state;
     }
 
+    public void playGame(){
+        // get shot information
+        // go through shot + action of saving
+    }
+
+    public void getShotInfo(){
+        state = 1;
+        // print instructions
+        // get speed meter
+
+    }
+
+    public void playShot(){
+        state = 2;
+        // move ball
+        if(xShotPos < ball.getX()){
+            ball.setDx(-ball.getDx());
+        }
+
+        if(yShotPos < ball.getY()){
+            ball.setDy(-ball.getDy());
+        }
+
+        while (ball.getX() + ball.getDx() < xShotPos)
+        ball.move();
+        // respond to keeper movements
+
+    }
+
     public static void main(String[] args) {
         Game g = new Game();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if(state == 1){
+            xShotPos = e.getX();
+            yShotPos = e.getY();
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(state == 2){
+            ball.move();
+        }
+
     }
 }
